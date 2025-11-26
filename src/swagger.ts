9bdccefd896839e79f9036a2f501d59a07a62426
pnkolom <mvogopierre129@gmail.com>
2025-11-26 15:19:17 +0100

    new feature

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,22 +1,22 @@
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const options: swaggerJSDoc.Options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Distribution Musicale',
-      version: '1.0.0',
+      title: "API Distribution Musicale",
+      version: "1.0.0",
       description:
         "Documentation interactive de l'API pour la plateforme de distribution musicale.",
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: "https://disktro-backend.onrender.com",
       },
     ],
   },
-  apis: ['./src/routes/*.ts', './src/swagger-components.ts'], // fichiers où Swagger ira lire les annotations
+  apis: ["./src/routes/*.ts", "./src/swagger-components.ts"], // fichiers où Swagger ira lire les annotations
 };
 
 export const swaggerSpec = swaggerJSDoc(options);
